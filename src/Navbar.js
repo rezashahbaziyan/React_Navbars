a325c287ab763d9c4f483dd9ecab43feacd900c6
ujin <rezashahtrader@gmail.com>
2023-05-18 05:05:50 +0330

    navlist

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,14 @@ export default function Navbar() {
     return (
         <div className="Navbar">
             <div className="Logo"></div>
-            <div className="Nav_List"></div>
+            <div className="Nav_List">
+                <ul className="List">
+                    <li className="List_Item">Home</li>
+                    <li className="List_Item">About</li>
+                    <li className="List_Item">Profile</li>
+                    <li className="List_Item">Contact Us</li>
+                </ul>
+            </div>
         </div>
     )
 }
