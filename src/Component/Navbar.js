12193076c93f72774c7504d45f48ba56e07e5988
ujin <rezashahtrader@gmail.com>
2023-05-18 06:29:18 +0330

    responsiv

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -2,21 +2,25 @@ import React from "react";
 import "./Navbar.css"
 
 export default function Navbar() {
-    function click() {
+    function open() {
         document.querySelector(".menu").style.display = "flex";
+    }
+    function close() {
+        document.querySelector(".menu").style.display = "none";
     }
     return (
         <div className="Navbar">
             <div className="Logo"></div>
             <div className="menu">
                 <ul className="List_menu">
+                    <li className="List_Item menu_close" onClick={close}>X</li>
                     <li className="List_Item">Home</li>
                     <li className="List_Item">About</li>
                     <li className="List_Item">Profile</li>
                     <li className="List_Item">Contact Us</li>
                 </ul>
             </div>
-            <div className="hamborger_menu_icon" onClick={click}></div>
+            <div className="hamborger_menu_icon" onClick={open}></div>
             <div className="Nav_List">
                 <ul className="List">
                     <li className="List_Item">Home</li>
